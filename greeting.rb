eeb5843842eca621d1b750b6e249be124b40ca92
P S <s.prerita@gmail.com>
2019-05-07 01:39:39 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,35 @@
+def greeting
+  puts "Hello World"
+end
+
+
+greeting
+greeting
+greeting
+greeting
+greeting
+
+num_of_attendees = 547
+puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
+
+answer = "< fill in your answer here >"
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
+
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+
+end
+
+6+3
+total=6+3
+
+
+
+
+
+
+
+
